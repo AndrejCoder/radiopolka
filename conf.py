b834fb9ba8f4fe44f53ba8c23a1e594d1824e430
Andrey Belov <abelov@lanit.ru>
2017-10-08 14:55:29 +0300

    first.

---
+++ conf.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'radiopolka_db',
+        'USER': 'radiopolka',
+        'PASSWORD': '123456',
+        'HOST': '',
+        'PORT': '',
+    }
+}
