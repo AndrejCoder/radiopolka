2a7fa4a1b73c9b4c4c7cf6a80a4cf1cadffa4bd0
Andrey Belov <abelov@lanit.ru>
2018-04-01 09:52:14 +0300

    Доработан wsgi.py.

--- radiopolka/wsgi.py
+++ radiopolka/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), os.pardir)
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "radiopolka.settings")
 
