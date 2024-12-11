11a898b35e8f547b2718b84f6cb213ff86bac2b9
Andrey Belov <andrejbelo@gmail.com>
2024-12-11 19:48:33 +0300

    Fix command lxrefresh.

--- linkexchange_django/management/commands/lxrefresh.py
+++ linkexchange_django/management/commands/lxrefresh.py
@@ -22,9 +22,7 @@
 
 import logging
 import sys
-from optparse import make_option
 
-from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 
 from linkexchange.clients import PageRequest
@@ -33,12 +31,12 @@ from linkexchange_django import support
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-            make_option('--host', '-d', dest = 'host',
-                help = 'Request host.'),
-            make_option('--uri', '-i', dest = 'uri',
-                help = 'Request URI.'),
-            )
+    # option_list = BaseCommand.option_list + (
+    #         make_option('--host', '-d', dest = 'host',
+    #             help = 'Request host.'),
+    #         make_option('--uri', '-i', dest = 'uri',
+    #             help = 'Request URI.'),
+    #         )
     help = "Force to refresh LinkExchange clients databases."
 
     def handle(self, *args, **options):
@@ -56,14 +54,18 @@ class Command(BaseCommand):
             sys.exit(1)
 
         if host is None:
-            if Site._meta.installed:
-                current_site = Site.objects.get_current()
-                if current_site is not None:
-                    host = current_site.domain
-            if host is None:
-                sys.stderr.write(self.style.ERROR("No host configured\n"))
-                sys.exit(1)
+            # if Site._meta.installed:
+            #     current_site = Site.objects.get_current()
+            #     if current_site is not None:
+            #         host = current_site.domain
+            # if host is None:
+            sys.stderr.write(self.style.ERROR("No host configured\n"))
+            sys.exit(1)
 
         page_request = PageRequest(host=host, uri=uri)
         support.platform.refresh_db(page_request)
+
+    def add_arguments(self, parser):
+        parser.add_argument('--host', '-d', dest='host', help='Request host.')
+        parser.add_argument('--uri', '-i', dest='uri', help='Request URI.')
 
